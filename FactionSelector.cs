78ffdb8136ecc48b0e19ff595edf3c53a62a679d
Eivind <ehaugw@gmail.com>
2023-01-08 00:05:15 +0100

    implement framework to determine player faction

---
+++ FactionSelector.cs
@@ -0,0 +1,22 @@
+﻿using InstanceIDs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crusader
+{
+    public static class FactionSelector
+    {
+        public static bool IsHolyMission(Character character)
+        {
+            return character?.Inventory?.QuestKnowledge?.IsItemLearned(IDs.questionsAndCorruptionID) ?? false;
+        }
+
+        public static bool IsBlueChamberCollective(Character character)
+        {
+            return character?.Inventory?.QuestKnowledge?.IsItemLearned(IDs.mixedLegaciesID) ?? false;
+        }
+    }
+}
