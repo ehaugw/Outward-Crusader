c5d9792f9ae1620a32b5e466cbe248e99890279b
Eivind <ehaugw@gmail.com>
2024-07-15 14:30:19 +0200

    fix(BlessedDetermination): don't subtract both burst of divinity and ancestral memory for the same mana

--- Spells/BlessedDeterminationSpell.cs
+++ Spells/BlessedDeterminationSpell.cs
@@ -179,6 +179,7 @@ namespace Crusader
                                     effect.RemoveOldestStack();
                                 }
                             }
+                            break;
                         }
                     }
                 }
